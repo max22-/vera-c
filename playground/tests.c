9140e0831a562928efd0a1ab3e98f009e71a02ba
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2024-12-09 23:54:43 +0100

    Added another test that fails, for tomorrow

--- playground/tests.c
+++ playground/tests.c
@@ -10,15 +10,17 @@ vera_allocator allocator;
 void test_interning(void) {
     vera_ctx ctx;
     vera_init_ctx(&ctx, allocator, "");
-    const char *str1 = "abc def", *str2 = "abc    def", *str3 = "abc de", *str4 = "efqhqi 12rq";
+    const char *str1 = "abc def", *str2 = "abc    def", *str3 = "abc de", *str4 = "efqhqi 12rq", *str5 = " abc def ";
     assert(vera_intern(&ctx, str1, strlen(str1)) == 0);
     assert(vera_intern(&ctx, str2, strlen(str2)) == 1);
     assert(vera_intern(&ctx, str3, strlen(str3)) == 2);
     assert(vera_intern(&ctx, str4, strlen(str4)) == 3);
+    assert(vera_intern(&ctx, str5, strlen(str5)) == 0);
     assert(vera_intern(&ctx, str1, strlen(str1)) == 0);
     assert(vera_intern(&ctx, str2, strlen(str2)) == 1);
     assert(vera_intern(&ctx, str3, strlen(str3)) == 2);
     assert(vera_intern(&ctx, str4, strlen(str4)) == 3);
+    assert(vera_intern(&ctx, str5, strlen(str5)) == 0);
 }
 
 int main(void) {
