11575607d3996d1a50117e3af107f425e1a977e4
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2024-12-10 11:37:12 +0100

    Corrected printf format string

--- playground/main.c
+++ playground/main.c
@@ -17,7 +17,7 @@ int main(void) {
     vera_ctx ctx;
     vera_init_ctx(&ctx, src, NULL, 0);
     size_t pool_size = vera_parse(&ctx);
-    printf("%d objects parsed\n", pool_size);
+    printf("%zu objects parsed\n", pool_size);
 
     size_t bytes = sizeof(vera_obj) * pool_size;
     printf("allocating %zu bytes\n", bytes);
